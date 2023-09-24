e2f8d76f7c158da273f95e640db6c6ecebe4afeb
selectjundev <selectjun.dev@gmail.com>
2023-09-24 11:08:35 +0900

    [ build ] spring boot/cloud 및 java 버전 업

--- src/main/java/com/attendancecheck/sample/SampleApplication.java
+++ src/main/java/com/attendancecheck/sample/SampleApplication.java
@@ -2,12 +2,12 @@ package com.attendancecheck.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @RefreshScope
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class SampleApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/attendancecheck/sample/utils/UserContextFilter.java
+++ src/main/java/com/attendancecheck/sample/utils/UserContextFilter.java
@@ -1,11 +1,11 @@
 package com.attendancecheck.sample.utils;
 
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 /**
