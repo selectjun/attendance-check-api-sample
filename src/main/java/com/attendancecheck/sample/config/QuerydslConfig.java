a083f350b385789171298a8d5531db8511aef891
selectjun <selectjun@gmail.com>
2023-10-12 20:08:50 +0900

    [ feat ] QueryDSL 의존성 추가

---
+++ src/main/java/com/attendancecheck/sample/config/QuerydslConfig.java
@@ -0,0 +1,26 @@
+package com.attendancecheck.sample.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Query DSL 설정
+ */
+@Configuration
+public class QuerydslConfig {
+
+    /**
+     * Entity Manager
+     */
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+
+}
